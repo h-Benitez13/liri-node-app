20b7154c2bfb3363769d0d74561930c64dfebb63
h-Benitez13 <hbenitez3711@gmail.com>
2019-01-25 23:06:31 -0500

    we back

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log("this is loaded");
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: "7da6885c3a6940e1853633d2e929620c",
+    secret: "58b9e747b97c4d769cbc70f565f381d9"
 };
 
--- liri.js
+++ liri.js
@@ -7,8 +7,10 @@ var axios = require("axios");
 var moment = require("moment");
 moment().format();
 
-var keys = require("keys");
-var spotify = new spotify(keys.spotify);
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
 
 
 var action = process.argv[2];
@@ -18,19 +20,70 @@ switch (input, action) {
     case "movie-this":
         if (input !== "") {
             movieTime(input);
-        } 
+        }
         else {
             input = "Mr. Nobody"
             movieTime(input);
         }
         break;
     case "concert-this":
-        band();
+        if(input !==""){
+            band();
+        }
+        else{
+            input= "Chainsmokers"
+            band();
+        }
         break;
     case "do-what-it-says":
         doWhat();
         break;
+    case "spotify-this-song":
+        if(input !== ""){
+            spofityPlay();
+        }else {
+            input="At your best"
+            spofityPlay();
+        }
+        break;
 }
+
+// spotify
+// make it work 
+function spofityPlay() {
+    spotify
+  .search({ type: 'track', query: input })
+  .then(function(data) {
+    console.log(
+        "\n ========== Spotify Search ==========\n"
+    )
+
+    // console.log(data);
+    // The song's name
+    // A preview link of the song from Spotify
+    // The album that the song is from
+    //Artist(s)
+    // console.log(data.tracks.items[1])
+    var song = data.tracks.items[1];
+    console.log("Song Search: " + song.album.name);
+    console.log("Want a quick listen, click here: " + song.external_urls.spotify)
+    console.log("The Song is in Album: " + song.album.name);
+    console.log("Artist(s): " +song.artists[0].name);
+    // ARTISTS VS ALBUM
+    // ALBUM: given to us as an object allows us to call on it normally
+    // ARISTS: looks like an object but its really an array with an object
+    // nested inside (sneaky bitch.)
+    // for the artist name you have to call the array FIRST then the object
+    
+})
+  .catch(function(err) {
+    return console.log('Error occurred: ' + err);
+
+  });
+   
+
+}
+
 
 // OMDB 
 // make it work then move down to the requirements 
@@ -39,7 +92,7 @@ function movieTime() {
 
     var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
 
-    
+
 
     axios
         .get(queryUrl)
@@ -47,24 +100,28 @@ function movieTime() {
         .then(
 
             function (response) {
-                var movieD =response.data;
-                
+                var movieD = response.data;
+                // console.log(response)
+                console.log(
+                    "\n ========== Movie Search ==========\n"
+                )
+
                 //    * Title of the movie.
-                console.log("Movie Title: " + movieD.Title 
-                //    * Year the movie came out.
-                          +  "\nYear the movie came out: " + movieD.Year
-                //    * IMDB Rating of the movie.
-                          +  "\nIMDB Rating: " + movieD.imdbRating
-                //    * Rotten Tomatoes Rating of the movie.
-                          +  "\nRotten Tomatoes Rating: " + movieD.Ratings[2].Value
-                //    * Country where the movie was produced.
-                          +  "\nThe country the movie was produced in: " + movieD.Country
-                //    * Language of the movie.
-                          +  "\nLanguage of the movie: " + movieD.Language
-                //    * Plot of the movie.
-                          +  "\nMovie Plot: " + movieD.Plot
-                //    * Actors in the movie.
-                          +  "\nActors/Actresses: " + movieD.Actors);
+                console.log("Movie Title: " + movieD.Title
+                    //    * Year the movie came out.
+                    + "\nYear the movie came out: " + movieD.Year
+                    //    * IMDB Rating of the movie.
+                    + "\nIMDB Rating: " + movieD.imdbRating
+                    //    * Rotten Tomatoes Rating of the movie.
+                    + "\nRotten Tomatoes Rating: " + movieD.Ratings[2].Value
+                    //    * Country where the movie was produced.
+                    + "\nThe country the movie was produced in: " + movieD.Country
+                    //    * Language of the movie.
+                    + "\nLanguage of the movie: " + movieD.Language
+                    //    * Plot of the movie.
+                    + "\nMovie Plot: " + movieD.Plot
+                    //    * Actors in the movie.
+                    + "\nActors/Actresses: " + movieD.Actors + "\n");
             }
         )
 }
@@ -82,12 +139,15 @@ function band() {
     axios
         .get(queryUrl)
         .then(function (bandResponse) {
+            console.log(
+                "\n ========== Band Search ==========\n"
+            )
 
-            // console.log(bandResponse.data[1]);
+            // console.log(bandResponse.data);
             var venueInfo = bandResponse.data[1];
             console.log("Name of the venue " + venueInfo.venue.name);
             console.log("Venue located at " + venueInfo.venue.city + "," + venueInfo.venue.region);
-            console.log("Event date " + moment(venueInfo.datetime).format("MM/DD/YYYY"));
+            console.log("Event date " + moment(venueInfo.datetime).format("MM/DD/YYYY")+"\n");
             // name of the venue
             // venue location 
             // date of the event (use moment to format this as "MM/DD/YYYY");
@@ -97,18 +157,32 @@ function band() {
 
 function doWhat() {
     fs.readFile("random.txt", "utf8", function (error, data) {
-        
+
+        console.log(
+            "\n ========== SURRRRPRISE ==========\n"
+        )
 
         if (error) {
             return console.log(" yo my man...wasss good " + error);
         }
 
-        // var dataArr = data.split(",");
-       console.log(data);
-       var userAction = data[1];
+            var dataArr = data.split(",");
+            
+            console.log(dataArr[2]);
+        
 
-        if (userAction === "movie-this") {
-            movieTime(userInput);
+        
+        
+       
+
+        if (input === 'spotify-this-song') {
+            spofityPlay();
+        }
+        if (input === "movie-this"){
+            movieTime();
+        }
+        if(input === "concert-this"){
+            band(dataArr[1]);
         }
     });
 }
